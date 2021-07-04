ee26f1542a909411680a2331ec8fc5036e42f0f6
karbo lei <karbo@lei.online>
2021-07-04 20:34:45 +0800

    integer builds tree, floating point queries

--- src/torch_kdtree.cu
+++ src/torch_kdtree.cu
@@ -47,20 +47,20 @@ public:
     refIdx_t root;
     KdNode* kdNodes;
     KdCoord* coordinates;
+    float* coordinates_float; // floating type for querying
     sint numPoints;
     sint numDimensions;
     bool is_cuda;
-    float scale, offset;
 
-    TorchKDTree(sint _numPoints, sint _numDimensions, float _scale, float _offset): 
+    TorchKDTree(sint _numPoints, sint _numDimensions): 
             root(-1), 
             numPoints(_numPoints), numDimensions(_numDimensions), 
-            is_cuda(true), 
-            scale(_scale), offset(_offset)
+            is_cuda(true)
     {
         kdNodes = new KdNode[_numPoints];
         coordinates = new KdCoord[_numPoints * _numDimensions];
-        if (kdNodes == nullptr || coordinates == nullptr)
+        coordinates_float = new float[_numPoints * _numDimensions];
+        if (kdNodes == nullptr || coordinates == nullptr || coordinates_float == nullptr)
         {
             throw runtime_error("error when allocating host memory");
         }
@@ -71,17 +71,17 @@ public:
         root = _tree.root; _tree.root = -1;
         kdNodes = _tree.kdNodes; _tree.kdNodes = nullptr;
         coordinates = _tree.coordinates; _tree.coordinates = nullptr;
+        coordinates_float = _tree.coordinates_float; _tree.coordinates_float = nullptr;
         numPoints = _tree.numPoints; _tree.numPoints = 0;
         numDimensions = _tree.numDimensions;
         is_cuda = _tree.is_cuda;
-        scale = _tree.scale;
-        offset = _tree.offset;
     }
 
     ~TorchKDTree()
     {
         delete[] kdNodes;
         delete[] coordinates;
+        delete[] coordinates_float;
     }
 
     std::string __repr__()
@@ -161,9 +161,9 @@ public:
     ////////////////////////////////////////////////////////////////////////////////////////////
     //////////////////////////////////// searching functions ///////////////////////////////////
     ////////////////////////////////////////////////////////////////////////////////////////////
-    refIdx_t _search(const KdCoord* query, refIdx_t node) // searching down the tree until reaching leaf node
+    refIdx_t _search(const float* query, refIdx_t node) // searching down the tree until reaching leaf node
     {
-        KdCoord val, val_node;
+        float val, val_node;
         bool has_left_node, has_right_node;
         while (true)
         {
@@ -176,7 +176,7 @@ public:
                 if (has_left_node && has_right_node)
                 {
                     val = query[node_current.split_dim];
-                    val_node = coordinates[numDimensions * node_current.tuple + node_current.split_dim];
+                    val_node = coordinates_float[numDimensions * node_current.tuple + node_current.split_dim];
                     if (val < val_node) node = node_current.ltChild;
                     else node = node_current.gtChild;
                 }
@@ -188,28 +188,29 @@ public:
         return node;
     }
 
-    inline KdCoord squared_distance(const KdCoord* point_a, const KdCoord* point_b) // squared distance
+    inline float distance(const float* point_a, const float* point_b)
     {
-        KdCoord sum = 0;
+        float sum = 0;
         for (sint i = 0; i < numDimensions; ++i)
             sum += POW2(point_a[i] - point_b[i]);
         return sum;
     }
 
-    inline KdCoord squared_distance_plane(const KdCoord* point, refIdx_t node) // to plane squared distance
+    inline float distance_plane(const float* point, refIdx_t node)
     {
         const KdNode& node_plane = kdNodes[node];
-        return POW2(point[node_plane.split_dim] - coordinates[numDimensions * node_plane.tuple + node_plane.split_dim]);
+        return POW2(point[node_plane.split_dim] - coordinates_float[numDimensions * node_plane.tuple + node_plane.split_dim]);
     }
 
     // search for a single point
-    void _search_nearest(const KdCoord* point, int64_t* out_)
+    // see: https://zhuanlan.zhihu.com/p/45346117
+    void _search_nearest(const float* point, int64_t* out_)
     {
         using start_end = std::tuple<refIdx_t, refIdx_t>;
 
-        KdCoord dist       = std::numeric_limits<KdCoord>::max();
-        KdCoord dist_plane = std::numeric_limits<KdCoord>::max();
-        KdCoord dist_best  = std::numeric_limits<KdCoord>::max();
+        float dist       = std::numeric_limits<float>::max();
+        float dist_plane = std::numeric_limits<float>::max();
+        float dist_best  = std::numeric_limits<float>::max();
         refIdx_t node_best, node_bro;
         
         // BFS
@@ -222,29 +223,34 @@ public:
             std::tie(node_start, node_end) = buffer.front();
             buffer.pop();
 
-            // update if current node is better
-            dist = squared_distance(point, coordinates + numDimensions * kdNodes[node_end].tuple);
-            if (dist < dist_best)
+            // back trace until to the starting node
+            while (true)
             {
-                dist_best = dist;
-                node_best = node_end;
-            }
-
-            // if intersect with plane, search another branch
-            dist_plane = squared_distance_plane(point, node_end);
-            if (dist_plane < dist_best)
-            {
-                node_bro = kdNodes[node_end].brother;
-                if (node_bro >= 0)
+                // update if current node is better
+                dist = distance(point, coordinates_float + numDimensions * kdNodes[node_end].tuple);
+                if (dist < dist_best)
                 {
-                    buffer.emplace(start_end(node_bro, _search(point, node_bro)));
+                    dist_best = dist;
+                    node_best = node_end;
                 }
-            }
 
-            // back trace
-            if (node_start != node_end)
-            {
-                buffer.emplace(start_end(node_start, kdNodes[node_end].parent));
+                if (node_end != node_start)
+                {
+                    node_bro = kdNodes[node_end].brother;
+                    if (node_bro >= 0)
+                    {
+                        // if intersect with plane, search another branch
+                        dist_plane = distance_plane(point, kdNodes[node_end].parent);
+                        if (dist_plane < dist_best)
+                        {
+                            buffer.emplace(start_end(node_bro, _search(point, node_bro)));
+                        }
+                    }
+
+                    // back trace
+                    node_end = kdNodes[node_end].parent;
+                }
+                else break;
             }
         }
 
@@ -257,8 +263,9 @@ public:
         CHECK_FLOAT32(points);
         TORCH_CHECK(points.size(1) == numDimensions, "dimensions mismatch");
         sint numQuery = points.size(0);
-        torch::Tensor points_int = torch::round(points * scale + offset).to(torch::kInt32);
-        KdCoord* points_int_ptr = points_int.data_ptr<KdCoord>();
+        // std::cout << "scale = " << scale << ", offset = " << offset <<std::endl;
+        // torch::Tensor points_int = torch::round(points * scale + offset).to(torch::kInt32);
+        float* points_ptr = points.data_ptr<float>();
         torch::Tensor indices_tensor;
 
         if (is_cuda)
@@ -274,7 +281,7 @@ public:
             #pragma omp parallel for
             for (sint i = 0; i < numQuery; ++i)
             {
-                _search_nearest(points_int_ptr + i * numDimensions, raw_ptr + i);
+                _search_nearest(points_ptr + i * numDimensions, raw_ptr + i);
             }
         }
 
@@ -299,22 +306,36 @@ const sint numBlocks     = 32;
     TODO  @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
     - if it is already on CUDA, do not copy to CPU
 */
-TorchKDTree torchBuildCUDAKDTree(torch::Tensor data, float scale, float offset)
+TorchKDTree torchBuildCUDAKDTree(torch::Tensor data_float)
 {
     // check input
-    CHECK_CONTIGUOUS(data);
-    CHECK_INT32(data);
-    sint numPoints = data.size(0);
-    sint numDimensions = data.size(1);
-    TorchKDTree tree(numPoints, numDimensions, scale, offset);
+    CHECK_CONTIGUOUS(data_float);
+    CHECK_FLOAT32(data_float);
+    sint numPoints = data_float.size(0);
+    sint numDimensions = data_float.size(1);
+
+    auto val_max = std::get<0>(torch::max(data_float, 0, false));
+    auto val_min = std::get<0>(torch::min(data_float, 0, false));
+    const float bound_scaling = 0.1;
+    float int_max = std::numeric_limits<KdCoord>::max() * bound_scaling;
+    float int_min = std::numeric_limits<KdCoord>::min() * bound_scaling;
+    auto scale  = float(int_max - int_min) / (val_max - val_min);
+    auto offset = -val_min * scale + int_min;
+    auto data = torch::round(data_float * scale + offset).to(torch::kInt32);
+
+    TorchKDTree tree(numPoints, numDimensions);
     if (data.is_cuda())
     {
         KdCoord* data_ptr = data.data_ptr<KdCoord>();
         gpuErrchk(cudaMemcpy(tree.coordinates, data_ptr, numPoints * numDimensions * sizeof(KdCoord), cudaMemcpyDeviceToHost)); // make a copy
+        float* float_ptr = data_float.data_ptr<float>();
+        gpuErrchk(cudaMemcpy(tree.coordinates_float, float_ptr, numPoints * numDimensions * sizeof(float), cudaMemcpyDeviceToHost)); // make a copy
     } else
     {
         KdCoord* data_ptr = data.data_ptr<KdCoord>();
         std::memcpy(tree.coordinates, data_ptr, numPoints * numDimensions * sizeof(KdCoord)); // make a copy
+        float* float_ptr = data_float.data_ptr<float>();
+        std::memcpy(tree.coordinates_float, float_ptr, numPoints * numDimensions * sizeof(float)); // make a copy
     }
 
     // initialize environment
@@ -374,8 +395,6 @@ PYBIND11_MODULE(TORCH_EXTENSION_NAME, m)
         .def_readonly("numPoints", &TorchKDTree::numPoints)
         .def_readonly("numDimensions", &TorchKDTree::numDimensions)
         .def_readonly("is_cuda", &TorchKDTree::is_cuda)
-        .def_readonly("scale", &TorchKDTree::scale)
-        .def_readonly("offset", &TorchKDTree::offset)
         .def("__repr__", &TorchKDTree::__repr__)
 
         // access node
--- test/test.py
+++ test/test.py
@@ -5,36 +5,28 @@ from scipy.spatial.ckdtree import cKDTreeNode
 from torch_cluster import knn
 from time import time
 
-# NOTE 3 * ((MAX - MIN) / K) ** 2 <= MAX     ==>   K >= 160529.75978303837
-# RANGE_MAX = torch.iinfo(torch.int32).max / 160530
-# RANGE_MIN = torch.iinfo(torch.int32).min / 160530
-RANGE_MAX = torch.iinfo(torch.int32).max / 160530
-RANGE_MIN = torch.iinfo(torch.int32).min / 160530
-
 
 def buildKDTree(data):
-    range_min, range_max = data.min(), data.max()
-    scale = (RANGE_MAX - RANGE_MIN) / (range_max - range_min)
-    offset = -range_min * scale + RANGE_MIN
-    data_input = torch.round(data * scale + offset).to(dtype=torch.int32)
-    tree = torchkdtree.torchBuildCUDAKDTree(data_input, scale, offset)
+    tree = torchkdtree.torchBuildCUDAKDTree(data)
     return tree
 
 
-
 if __name__ == "__main__":
-    data = torch.round(torch.randn(65536, 3) * 1024) / 1024
+    data = torch.randn(65536, 3)
+    # data = torch.round(torch.randn(65536, 10) * 4096).float()
     tree = buildKDTree(data)
     tree.cpu().verify()
 
 
-    query = torch.round(torch.randn(65536, 3) * 1024) / 1024
+    query = torch.randn(65536, 3)
+    # query = torch.round(torch.randn(65536, 10) * 4096).float()
     
+
     t0 = time()
     index = tree.search_nearest(query)
     print(f"time = {time() - t0}")
     import ipdb; ipdb.set_trace()
-    
+
     index_gt = knn(data, query, k=1)
     index_gt = index_gt[1][torch.argsort(index_gt[0])]
     wrong_loc = torch.where(index != index_gt)[0]
