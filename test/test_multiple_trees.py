2470daecabe22d4a112fb6f1c2600ba147ddc1f5
karbo lei <karbo@lei.online>
2021-07-09 22:31:29 +0800

    TODO the second card does not run

--- test/test_multiple_trees.py
+++ test/test_multiple_trees.py
@@ -11,11 +11,13 @@ if __name__ == "__main__":
     tree = torch_kdtree.torchBuildCUDAKDTree(torch.randn([NUM, 3], device="cuda:0"))
     tree.cpu()
     index, batch = tree.search_radius(torch.randn(NUM, 3), RADIUS)
-    
+    print("finished 1")
+
     ########################################
-    tree2 = torch_kdtree.torchBuildCUDAKDTree(torch.randn([NUM, 3], device="cuda:1"))
+    tree2 = torch_kdtree.torchBuildCUDAKDTree(torch.randn([NUM, 3], device="cuda:1")) ###### the second card does not run
     tree2.cpu()
     index2, batch2 = tree2.search_radius(torch.randn(NUM, 3), RADIUS)
+    print("finished 2")
 
     import ipdb; ipdb.set_trace()
 
